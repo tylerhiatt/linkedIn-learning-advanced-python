63d6523692bfc8fd7a93e0de43a1dc9176ea298d
Tyler Hiatt <tyler.hiatt@ncino.com>
2024-08-21 11:22:29 -0600

    Solution to average race times

--- challenge/average_race_time.py
+++ challenge/average_race_time.py
@@ -3,17 +3,26 @@
 
 import re
 import datetime
+import pandas as pd
+import numpy as np
 
 def get_data():
     """Return content from the 10k_racetimes.txt file"""
-    with open('10k_racetimes.txt', 'rt') as file:
-        content = file.read()
-    return content
+    # with open('10k_racetimes.txt', 'rt') as file:
+    #     content = file.read()
+    # return content
+    df = pd.read_csv("10k_racetimes.csv")
+    return df
 
 def get_rhines_times():
     """Return a list of Jennifer Rhines' race times"""
-    races = get_data()
-    pass
+    races_df = get_data()
+    # TODO: grab only Rhines times by searching athlete column for her name
+        # create a list of her times
+    filtered_df = races_df[races_df['Athlete'].str.contains('Jennifer Rhines', case=False, na=False)]
+    times_list = filtered_df['TIME'].to_list()
+    
+    return times_list
 
 def get_average():
     """Return Jennifer Rhines' average race time in the format:
@@ -22,4 +31,16 @@ def get_average():
        s corresponds to a seconds digit
        M corresponds to a milliseconds digit (no rounding, just the single digit)"""
     racetimes = get_rhines_times()
-    pass
+    total = datetime.timedelta()
+
+    for time in racetimes:
+        try:
+            mins, secs, ms = re.split(r'[:.]', time)
+            total += datetime.timedelta(minutes=int(mins), seconds=int(secs), milliseconds=int(ms))
+        except ValueError:
+            mins, secs = re.split(r'[:.]', time)
+            total += datetime.timedelta(minutes=int(mins), seconds=int(secs))
+
+    avg_times = total / len(racetimes)
+    print(avg_times)
+    return f'{avg_times}'[2:-5]
